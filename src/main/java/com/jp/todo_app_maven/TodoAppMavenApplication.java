ab70d341ec446fcc21e8322760753a51606804b3
Akito070 <68894939+Akito070@users.noreply.github.com>
2025-12-28 13:13:27 +0900

    first commit

---
+++ src/main/java/com/jp/todo_app_maven/TodoAppMavenApplication.java
@@ -0,0 +1,13 @@
+package com.jp.todo_app_maven;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoAppMavenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodoAppMavenApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jp/todo_app_maven/TodoAppMavenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jp.todo_app_maven;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoAppMavenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
