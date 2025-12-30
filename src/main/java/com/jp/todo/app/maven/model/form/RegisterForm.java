e718b8b4df71f587dfa0468b2742bea5730a4149
Akito070 <68894939+Akito070@users.noreply.github.com>
2025-12-31 00:43:49 +0900

    ユーザ登録機能実装完了

---
+++ src/main/java/com/jp/todo/app/maven/model/form/RegisterForm.java
@@ -0,0 +1,24 @@
+package com.jp.todo.app.maven.model.form;
+
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
+
+@Data
+public class RegisterForm{
+
+    // 氏名
+	@NotEmpty(message = "{NotBlank.registerForm.name.ja}")
+	@Size(max = 100, message = "{Size.registerForm.name.ja}")
+    private String name; 
+    
+    // ユーザ名
+    @NotEmpty(message = "{NotBlank.registerForm.userName.ja}")
+    @Size(max = 100, message = "{Size.registerForm.userName.ja}")
+    private String userName; 
+
+    // パスワード
+    @NotEmpty(message = "{NotBlank.registerForm.password.ja}")
+    @Size(min = 6, message = "{Size.registerForm.password.ja}")
+    private String password; 
+}
